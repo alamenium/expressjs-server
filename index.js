f6d7e19eca4eefa4dfa853062a0de54f995237f8
Alamenium <39162837+alamenium@users.noreply.github.com>
2023-06-09 23:08:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
-import express from 'express';
+const express = require('express');
+const sgMail = require('@sendgrid/mail');
+
+sgMail.setApiKey('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');
+
 const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Choo Choo! Welcome to your Express app 🚅');
-})
+// API endpoint to send emails
+app.post('/send-mail', (req, res) => {
+  const { email, subject, message } = req.body;
 
-app.get("/json", (req, res) => {
-    res.json({"Choo Choo": "Welcome to your Express app 🚅"});
-})
+  const msg = {
+    to: email,
+    from: 'youssifayman2004@gmail.com',
+    subject,
+    html: `<strong>${message}</strong>`,
+  };
 
-const port = process.env.PORT || 3000;
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log('Email sent');
+      res.sendStatus(200);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.sendStatus(500);
+    });
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Server is running on port ${port}`);
+});
